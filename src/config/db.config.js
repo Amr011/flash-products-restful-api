d480745601bad94379b903e3f844746bd4269c59
Amr109 <grand.109.master@gmail.com>
2021-10-16 18:38:45 +0300

    ❄ dockerize app

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,17 +2,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const developmentConfig = {
-  HOST: process.env.HOST,
-  USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD,
-  DB: process.env.DB,
-  DIALECT: process.env.DIALECT,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+   MYSQL_HOST: process.env.HOST,
+   MYSQL_USER: process.env.USER,
+   MYSQL_PASSWORD: process.env.PASSWORD,
+   MYSQL_DB: process.env.DB,
+   MYSQL_DIALECT: process.env.DIALECT,
+   pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+   },
 };
 
 module.exports = developmentConfig;
